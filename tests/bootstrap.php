19679d111f1656525d0d43dc7a25c0c203d4c26b
Arkadiusz Moskwa <a.moskwa@gmail.com>
2014-09-27 00:14:23 +0200

    adding bootstrap for test, modifying paths for building

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * bootstrap.php
+ *
+ * Creation date: 2014-09-26
+ * Creation time: 22:53
+ *
+ * @author Arkadiusz Moskwa <a.moskwa@gmail.com>
+ */
+
+require_once implode('/', [__DIR__, '..', 'vendor', 'autoload.php']);
