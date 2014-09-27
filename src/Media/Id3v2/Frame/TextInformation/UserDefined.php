cda59866e9ae6204f79ab5181427367fde8dfe53
Arkadiusz Moskwa <a.moskwa@gmail.com>
2014-09-27 19:45:53 +0200

    user defined text frame

--- src/Media/Id3v2/Frame/TextInformation/UserDefined.php
+++ src/Media/Id3v2/Frame/TextInformation/UserDefined.php
@@ -10,8 +10,49 @@
 
 namespace Nakard\MusicFormats\Media\Id3v2\Frame\TextInformation;
 
-
-class UserDefined 
+/**
+ * Class UserDefined
+ *
+ * @package Nakard\MusicFormats\Media\Id3v2\Frame\TextInformation
+ */
+class UserDefined extends AbstractFrame
 {
+    protected $identifier = 'TXXX';
 
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * Constructs new user defined text information frame
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->description = '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @throws \InvalidArgumentException
+     * @return UserDefined
+     */
+    public function setDescription($description)
+    {
+        if (!is_string($description)) {
+            throw new \InvalidArgumentException('Description must be a string');
+        }
+        $this->description = $description;
+
+        return $this;
+    }
 } 
--- tests/Media/Id3v2/Frame/TextInformation/UserDefinedTest.php
+++ tests/Media/Id3v2/Frame/TextInformation/UserDefinedTest.php
@@ -10,8 +10,44 @@
 
 namespace Nakard\MusicFormats\Tests\Media\Id3v2\Frame\TextInformation;
 
+use Nakard\MusicFormats\Media\Id3v2\Frame\TextInformation\UserDefined;
 
-class UserDefinedTest 
+class UserDefinedTest extends AbstractFrameTestCase
 {
+    /**
+     * @var UserDefined
+     */
+    protected $frame;
 
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->frame = new UserDefined();
+    }
+
+    public function testGetIdentifier()
+    {
+        $this->assertSame('TXXX', $this->frame->getIdentifier());
+    }
+
+    public function testGetDescription()
+    {
+        $this->assertSame('', $this->frame->getDescription());
+    }
+
+    public function testSetDescription()
+    {
+        $this->frame->setDescription('test description');
+        $this->assertSame('test description', $this->frame->getDescription());
+    }
+
+    /**
+     * @dataProvider exceptionForOnlyStringProvider
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Description must be a string
+     */
+    public function testSetDescriptionWithInvalidArgument($argument)
+    {
+        $this->frame->setDescription($argument);
+    }
 } 
