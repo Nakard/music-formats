53747d54448c2e4a48c1cfe07a92c9c8d7c78881
Arkadiusz Moskwa <a.moskwa@gmail.com>
2014-09-28 16:24:31 +0200

    full tests for mpeg location lookup table

--- src/Media/Id3v2/Frame/MpegLocationLookupTable.php
+++ src/Media/Id3v2/Frame/MpegLocationLookupTable.php
@@ -10,8 +10,194 @@
 
 namespace Nakard\MusicFormats\Media\Id3v2\Frame;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Nakard\MusicFormats\Media\Id3v2\Frame\Reference\Reference;
 
-class MpegLocationLookupTable 
+/**
+ * Class MpegLocationLookupTable
+ *
+ * @package Nakard\MusicFormats\Media\Id3v2\Frame
+ */
+class MpegLocationLookupTable extends AbstractFrame
 {
+    protected $identifier = 'MLLT';
 
-} 
+    /**
+     * @var int
+     */
+    private $mpegFramesBetweenReference;
+
+    /**
+     * @var int
+     */
+    private $bytesBetweenReference;
+
+    /**
+     * @var int
+     */
+    private $msBetweenReference;
+
+    /**
+     * @var int
+     */
+    private $bitsNumberBytesDeviation;
+
+    /**
+     * @var int
+     */
+    private $bitsNumberMsDeviation;
+
+    /**
+     * @var ArrayCollection
+     */
+    private $references;
+
+    /**
+     * Construct new mpeg lookup table
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->mpegFramesBetweenReference = 0x0000;
+        $this->bytesBetweenReference = 0x000000;
+        $this->msBetweenReference = 0x000000;
+        $this->bitsNumberBytesDeviation = 0x00;
+        $this->bitsNumberMsDeviation = 0x00;
+        $this->references = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getBitsNumberBytesDeviation()
+    {
+        return $this->bitsNumberBytesDeviation;
+    }
+
+    /**
+     * @param int $bitsNumberBytesDeviation
+     * @throws \InvalidArgumentException
+     * @return MpegLocationLookupTable
+     */
+    public function setBitsNumberBytesDeviation($bitsNumberBytesDeviation)
+    {
+        if (!is_int($bitsNumberBytesDeviation)) {
+            throw new \InvalidArgumentException('Bytes deviation bits number must be an integer');
+        }
+        $this->bitsNumberBytesDeviation = $bitsNumberBytesDeviation;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBitsNumberMsDeviation()
+    {
+        return $this->bitsNumberMsDeviation;
+    }
+
+    /**
+     * @param int $bitsNumberMsDeviation
+     * @throws \InvalidArgumentException
+     * @return MpegLocationLookupTable
+     */
+    public function setBitsNumberMsDeviation($bitsNumberMsDeviation)
+    {
+        if (!is_int($bitsNumberMsDeviation)) {
+            throw new \InvalidArgumentException('Milisecond deviation bits number must be an integer');
+        }
+        $this->bitsNumberMsDeviation = $bitsNumberMsDeviation;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBytesBetweenReference()
+    {
+        return $this->bytesBetweenReference;
+    }
+
+    /**
+     * @param int $bytesBetweenReference
+     * @throws \InvalidArgumentException
+     * @return MpegLocationLookupTable
+     */
+    public function setBytesBetweenReference($bytesBetweenReference)
+    {
+        if (!is_int($bytesBetweenReference)) {
+            throw new \InvalidArgumentException('Bytes between reference must be an integer');
+        }
+        $this->bytesBetweenReference = $bytesBetweenReference;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMpegFramesBetweenReference()
+    {
+        return $this->mpegFramesBetweenReference;
+    }
+
+    /**
+     * @param int $mpegFramesBetweenReference
+     * @throws \InvalidArgumentException
+     * @return MpegLocationLookupTable
+     */
+    public function setMpegFramesBetweenReference($mpegFramesBetweenReference)
+    {
+        if (!is_int($mpegFramesBetweenReference)) {
+            throw new \InvalidArgumentException('MPEG frames between reference must be an integer');
+        }
+        $this->mpegFramesBetweenReference = $mpegFramesBetweenReference;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMsBetweenReference()
+    {
+        return $this->msBetweenReference;
+    }
+
+    /**
+     * @param int $msBetweenReference
+     * @throws \InvalidArgumentException
+     * @return MpegLocationLookupTable
+     */
+    public function setMsBetweenReference($msBetweenReference)
+    {
+        if (!is_int($msBetweenReference)) {
+            throw new \InvalidArgumentException('Miliseconds between reference must be an integer');
+        }
+        $this->msBetweenReference = $msBetweenReference;
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getReferences()
+    {
+        return $this->references;
+    }
+
+    /**
+     * @param Reference $reference
+     *
+     * @return MpegLocationLookupTable
+     */
+    public function addReference(Reference $reference)
+    {
+        $this->references->add($reference);
+
+        return $this;
+    }
+}
--- tests/Media/Id3v2/Frame/MpegLocationLookupTableTest.php
+++ tests/Media/Id3v2/Frame/MpegLocationLookupTableTest.php
@@ -10,9 +10,156 @@
 
 namespace Nakard\MusicFormats\Tests\Media\Id3v2\Frame;
 
+use Nakard\MusicFormats\Media\Id3v2\Frame\MpegLocationLookupTable;
+use Nakard\MusicFormats\Media\Id3v2\Frame\Reference\Reference;
 
-class MpegLocationLookupTableTest 
+/**
+ * Class MpegLocationLookupTableTest
+ *
+ * @package Nakard\MusicFormats\Tests\Media\Id3v2\Frame
+ */
+class MpegLocationLookupTableTest extends AbstractFrameTestCase
 {
+    /**
+     * @var MpegLocationLookupTable
+     */
+    protected $frame;
 
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->frame = new MpegLocationLookupTable();
+    }
+
+    public function testGetIdentifier()
+    {
+        $this->assertSame('MLLT', $this->frame->getIdentifier());
+    }
+
+    public function testGetMpegFramesBetweenReference()
+    {
+        $this->assertSame(0x0000, $this->frame->getMpegFramesBetweenReference());
+    }
+
+    public function testSetMpegFramesBetweenReference()
+    {
+        $this->frame->setMpegFramesBetweenReference(0x0004);
+        $this->assertSame(0x0004, $this->frame->getMpegFramesBetweenReference());
+    }
+
+    /**
+     * @dataProvider exceptionForOnlyIntegerProvider
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage MPEG frames between reference must be an integer
+     */
+    public function testSetMpegFramesBetweenReferenceWithInvalidArgument($argument)
+    {
+        $this->frame->setMpegFramesBetweenReference($argument);
+    }
+
+    public function testGetBytesBetweenReference()
+    {
+        $this->assertSame(0x0000, $this->frame->getBytesBetweenReference());
+    }
+
+    public function testSetBytesBetweenReference()
+    {
+        $this->frame->setBytesBetweenReference(0x0004);
+        $this->assertSame(0x0004, $this->frame->getBytesBetweenReference());
+    }
+
+    /**
+     * @dataProvider exceptionForOnlyIntegerProvider
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Bytes between reference must be an integer
+     */
+    public function testSetBytesBetweenReferenceWithInvalidArgument($argument)
+    {
+        $this->frame->setBytesBetweenReference($argument);
+    }
+
+    public function testGetMsBetweenReference()
+    {
+        $this->assertSame(0x0000, $this->frame->getMsBetweenReference());
+    }
+
+    public function testSetMsBetweenReference()
+    {
+        $this->frame->setMsBetweenReference(0x0004);
+        $this->assertSame(0x0004, $this->frame->getMsBetweenReference());
+    }
+
+    /**
+     * @dataProvider exceptionForOnlyIntegerProvider
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Miliseconds between reference must be an integer
+     */
+    public function testSetMsBetweenReferenceWithInvalidArgument($argument)
+    {
+        $this->frame->setMsBetweenReference($argument);
+    }
+
+    public function testGetBitsNumberBytesDeviation()
+    {
+        $this->assertSame(0x0000, $this->frame->getBitsNumberBytesDeviation());
+    }
+
+    public function testSetBitsNumberBytesDeviation()
+    {
+        $this->frame->setBitsNumberBytesDeviation(0x0004);
+        $this->assertSame(0x0004, $this->frame->getBitsNumberBytesDeviation());
+    }
+
+    /**
+     * @dataProvider exceptionForOnlyIntegerProvider
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Bytes deviation bits number must be an integer
+     */
+    public function testSetBitsNumberBytesDeviationWithInvalidArgument($argument)
+    {
+        $this->frame->setBitsNumberBytesDeviation($argument);
+    }
+
+    public function testGetBitsNumberMsDeviation()
+    {
+        $this->assertSame(0x0000, $this->frame->getBitsNumberMsDeviation());
+    }
+
+    public function testSetBitsNumberMsDeviation()
+    {
+        $this->frame->setBitsNumberMsDeviation(0x0004);
+        $this->assertSame(0x0004, $this->frame->getBitsNumberMsDeviation());
+    }
+
+    /**
+     * @dataProvider exceptionForOnlyIntegerProvider
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Milisecond deviation bits number must be an integer
+     */
+    public function testSetBitsNumberMsDeviationWithInvalidArgument($argument)
+    {
+        $this->frame->setBitsNumberMsDeviation($argument);
+    }
+
+    public function testGetReferences()
+    {
+        $this->assertEmpty($this->frame->getReferences());
+    }
+
+    public function testAddReference()
+    {
+        $reference = new Reference(0x04, 0x0c);
+        $this->frame->addReference($reference);
+        $this->assertEquals($reference, $this->frame->getReferences()->get(0));
+        $this->assertCount(1, $this->frame->getReferences());
+    }
+
+    /**
+     * @expectedException \ErrorException
+     */
+    public function testAddReferenceWithInvalidArgument()
+    {
+        $this->frame->addReference($this->frame);
+    }
 }
  
