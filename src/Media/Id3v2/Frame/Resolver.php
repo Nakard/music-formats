e1cbbc39c4c6158da04ec0179d24f1aedfdd14a5
Arkadiusz Moskwa <a.moskwa@gmail.com>
2014-09-28 13:54:38 +0200

    filling up resolver test

--- src/Media/Id3v2/Frame/Resolver.php
+++ src/Media/Id3v2/Frame/Resolver.php
@@ -21,8 +21,64 @@ use Nakard\MusicFormats\Media\Id3v2\Frame\Exception\InvalidIdentifierException;
 class Resolver
 {
     private $mapping = [
-        'UFID'  =>  '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UniqueFileIdentifier',
-        'MCDI'  =>  '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\MusicCdIdentifier',
+        'UFID'  =>  'UniqueFileIdentifier',
+        'MCDI'  =>  'MusicCdIdentifier',
+        'ETCO'  =>  'EventTimingCodes',
+        'TBPM'  =>  'TextInformation\\Derived\\BeatsPerMinute',
+        'TCON'  =>  'TextInformation\\Derived\\ContentType',
+        'TFLT'  =>  'TextInformation\\Derived\\FileType',
+        'TKEY'  =>  'TextInformation\\Derived\\InitialKey',
+        'TLAN'  =>  'TextInformation\\Derived\\Language',
+        'TLEN'  =>  'TextInformation\\Derived\\Length',
+        'TMED'  =>  'TextInformation\\Derived\\MediaType',
+        'TMOO'  =>  'TextInformation\\Derived\\Mood',
+        'TALB'  =>  'TextInformation\\Identification\\Album',
+        'TIT1'  =>  'TextInformation\\Identification\\ContentGroupDescription',
+        'TSRC'  =>  'TextInformation\\Identification\\InternationalStandardRecordingCode',
+        'TOAL'  =>  'TextInformation\\Identification\\OriginalAlbum',
+        'TPOS'  =>  'TextInformation\\Identification\\SetPart',
+        'TSST'  =>  'TextInformation\\Identification\\SetSubtitle',
+        'TIT3'  =>  'TextInformation\\Identification\\Subtitle',
+        'TIT2'  =>  'TextInformation\\Identification\\Title',
+        'TRCK'  =>  'TextInformation\\Identification\\TrackNumber',
+        'TPE2'  =>  'TextInformation\\InvolvedPersons\\Accompaniment',
+        'TPE1'  =>  'TextInformation\\InvolvedPersons\\Artist',
+        'TCOM'  =>  'TextInformation\\InvolvedPersons\\Composer',
+        'TPE3'  =>  'TextInformation\\InvolvedPersons\\Conductor',
+        'TENC'  =>  'TextInformation\\InvolvedPersons\\Encoder',
+        'TIPL'  =>  'TextInformation\\InvolvedPersons\\InvolvedPeople',
+        'TEXT'  =>  'TextInformation\\InvolvedPersons\\Lyricist',
+        'TMCL'  =>  'TextInformation\\InvolvedPersons\\MusicianCredits',
+        'TOPE'  =>  'TextInformation\\InvolvedPersons\\OriginalArtist',
+        'TOLY'  =>  'TextInformation\\InvolvedPersons\\OriginalLyricist',
+        'TPE4'  =>  'TextInformation\\InvolvedPersons\\Remixed',
+        'TCOP'  =>  'TextInformation\\License\\Copyright',
+        'TRSN'  =>  'TextInformation\\License\\InternetStation',
+        'TRSO'  =>  'TextInformation\\License\\InternetStationOwner',
+        'TOWN'  =>  'TextInformation\\License\\Owner',
+        'TPRO'  =>  'TextInformation\\License\\Produced',
+        'TPUB'  =>  'TextInformation\\License\\Publisher',
+        'TSOA'  =>  'TextInformation\\Other\\AlbumSortOrder',
+        'TSSE'  =>  'TextInformation\\Other\\EncodingSettings',
+        'TDEN'  =>  'TextInformation\\Other\\EncodingTime',
+        'TOFN'  =>  'TextInformation\\Other\\OriginalFilename',
+        'TDOR'  =>  'TextInformation\\Other\\OriginalReleaseTime',
+        'TSOP'  =>  'TextInformation\\Other\\PerformerSortOrder',
+        'TDLY'  =>  'TextInformation\\Other\\PlaylistDelay',
+        'TDRC'  =>  'TextInformation\\Other\\RecordingTime',
+        'TDRL'  =>  'TextInformation\\Other\\ReleaseTime',
+        'TDTG'  =>  'TextInformation\\Other\\TaggingTime',
+        'TSOT'  =>  'TextInformation\\Other\\TitleSortOrder',
+        'TXXX'  =>  'TextInformation\\UserDefined',
+        'WXXX'  =>  'UrlLink\\UserDefined',
+        'WCOM'  =>  'UrlLink\\CommercialInformation',
+        'WCOP'  =>  'UrlLink\\LegalInformation',
+        'WOAR'  =>  'UrlLink\\OfficialArtistWebpage',
+        'WOAF'  =>  'UrlLink\\OfficialAudioFileWebpage',
+        'WOAS'  =>  'UrlLink\\OfficialAudioSourceWebpage',
+        'WORS'  =>  'UrlLink\\OfficialInternetRadioWebpage',
+        'WPAY'  =>  'UrlLink\\Payment',
+        'WPUB'  =>  'UrlLink\\PublishersOfficialWebpage',
     ];
     /**
      * @param   string       $identifier
@@ -47,6 +103,7 @@ class Resolver
         if (!isset($this->mapping[$id])) {
             return new Unknown();
         }
-        return new $this->mapping[$id]();
+        $classFQCN = '\\' . __NAMESPACE__ . '\\' . $this->mapping[$id];
+        return new $classFQCN();
     }
 }
--- tests/Media/Id3v2/Frame/ResolverTest.php
+++ tests/Media/Id3v2/Frame/ResolverTest.php
@@ -11,8 +11,6 @@
 namespace Nakard\MusicFormats\Tests\Media\Id3v2\Frame;
 
 use Nakard\MusicFormats\Media\Id3v2\Frame\Resolver;
-use PhpBinaryReader\BinaryReader;
-use Nakard\MusicFormats\Media\Id3v2\Frame\Exception\InvalidIdentifierException;
 
 /**
  * Class ResolverTest
@@ -85,8 +83,66 @@ class ResolverTest extends \Nakard\MusicFormats\Tests\Media\Id3v2\AbstractTestCa
     {
         return [
             ['ABCD', 'Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\Unknown'],
-            ['UFID', 'Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UniqueFileIdentifier'],
-            ['MCDI', 'Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\MusicCdIdentifier']
+            ['UFID', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UniqueFileIdentifier'],
+            ['MCDI', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\MusicCdIdentifier'],
+            ['ETCO', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\EventTimingCodes'],
+            ['TBPM', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Derived\\BeatsPerMinute'],
+            ['TCON', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Derived\\ContentType'],
+            ['TFLT', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Derived\\FileType'],
+            ['TKEY', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Derived\\InitialKey'],
+            ['TLAN', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Derived\\Language'],
+            ['TLEN', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Derived\\Length'],
+            ['TMED', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Derived\\MediaType'],
+            ['TMOO', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Derived\\Mood'],
+            ['TALB', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Identification\\Album'],
+            ['TIT1', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Identification' .
+        '\\ContentGroupDescription'],
+            ['TSRC', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Identification' .
+        '\\InternationalStandardRecordingCode'],
+            ['TOAL', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Identification\\OriginalAlbum'],
+            ['TPOS', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Identification\\SetPart'],
+            ['TSST', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Identification\\SetSubtitle'],
+            ['TIT3', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Identification\\Subtitle'],
+            ['TIT2', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Identification\\Title'],
+            ['TRCK', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Identification\\TrackNumber'],
+            ['TPE2', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\Accompaniment'],
+            ['TPE1', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\Artist'],
+            ['TCOM', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\Composer'],
+            ['TPE3', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\Conductor'],
+            ['TENC', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\Encoder'],
+            ['TIPL', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\InvolvedPeople'],
+            ['TEXT', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\Lyricist'],
+            ['TMCL', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\MusicianCredits'],
+            ['TOPE', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\OriginalArtist'],
+            ['TOLY', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\OriginalLyricist'],
+            ['TPE4', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\InvolvedPersons\\Remixed'],
+            ['TCOP', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\License\\Copyright'],
+            ['TRSN', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\License\\InternetStation'],
+            ['TRSO', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\License\\InternetStationOwner'],
+            ['TOWN', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\License\\Owner'],
+            ['TPRO', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\License\\Produced'],
+            ['TPUB', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\License\\Publisher'],
+            ['TSOA', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\AlbumSortOrder'],
+            ['TSSE', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\EncodingSettings'],
+            ['TDEN', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\EncodingTime'],
+            ['TOFN', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\OriginalFilename'],
+            ['TDOR', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\OriginalReleaseTime'],
+            ['TSOP', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\PerformerSortOrder'],
+            ['TDLY', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\PlaylistDelay'],
+            ['TDRC', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\RecordingTime'],
+            ['TDRL', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\ReleaseTime'],
+            ['TDTG', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\TaggingTime'],
+            ['TSOT', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\Other\\TitleSortOrder'],
+            ['TXXX', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\TextInformation\\UserDefined'],
+            ['WXXX', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UrlLink\\UserDefined'],
+            ['WCOM', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UrlLink\\CommercialInformation'],
+            ['WCOP', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UrlLink\\LegalInformation'],
+            ['WOAR', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UrlLink\\OfficialArtistWebpage'],
+            ['WOAF', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UrlLink\\OfficialAudioFileWebpage'],
+            ['WOAS', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UrlLink\\OfficialAudioSourceWebpage'],
+            ['WORS', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UrlLink\\OfficialInternetRadioWebpage'],
+            ['WPAY', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UrlLink\\Payment'],
+            ['WPUB', '\\Nakard\\MusicFormats\\Media\\Id3v2\\Frame\\UrlLink\\PublishersOfficialWebpage'],
         ];
     }
 } 
