c2733acc666cb7c2a03b8f6933261c535a34633b
Arkadiusz Moskwa <a.moskwa@gmail.com>
2014-09-28 15:20:11 +0200

    adding new frames for incoming resolver in tests

---
+++ src/Media/Id3v2/Frame/MpegLocationLookupTable.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * MpegLocationLookupTable.php
+ *
+ * Creation date: 2014-09-28
+ * Creation time: 14:58
+ *
+ * @author Arkadiusz Moskwa <a.moskwa@gmail.com>
+ */
+
+namespace Nakard\MusicFormats\Media\Id3v2\Frame;
+
+
+class MpegLocationLookupTable 
+{
+
+} 
